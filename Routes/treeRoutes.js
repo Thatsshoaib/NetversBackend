d7f53eb403c1d945771aee109cee3d5f2d3b3ad9
Shoaib raza <133660631+Thatsshoaib@users.noreply.github.com>
2025-04-15 13:30:31 +0530

    Update treeRoutes.js

--- Routes/treeRoutes.js
+++ Routes/treeRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../config/db");
+const db = require("../Config/db");
 
 // GET XML-based user tree
 router.get("/tree/:userId/:planId", async (req, res) => {
