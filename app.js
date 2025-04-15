0363526da9cd5d4c8c5950f1253b405ed7cce0ba
Shoaib raza <133660631+Thatsshoaib@users.noreply.github.com>
2025-04-15 16:21:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,11 +19,14 @@ const path = require('path');
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'dist')));
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 // app.get('/', (req, res) => {
 //   res.send('hello, Welcome');
 // });
-
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 app.use("/api/auth", authRoutes);
 app.use("/api", treeRoutes);
 app.use("/api/users", sponsorRoutes);
