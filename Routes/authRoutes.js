ce1424cc4fc76c71a33e872374c79130165e8351
Shoaib raza <133660631+Thatsshoaib@users.noreply.github.com>
2025-04-15 13:52:56 +0530

    Update authRoutes.js

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const db = require("../config/db"); // ✅ Import the fixed MySQL connection
+const db = require("../Config/db"); // ✅ Import the fixed MySQL connection
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
