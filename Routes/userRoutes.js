51655e72a273b03ac5c163ba04c3cf39b44ddf17
Shoaib raza <133660631+Thatsshoaib@users.noreply.github.com>
2025-05-09 15:31:05 +0530

    Update userRoutes.js

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../config/db");
+const db = require("../Config/db");
 
 router.get("/", async (req, res) => {
   try {
