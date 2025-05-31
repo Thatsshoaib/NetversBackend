a9f597df281f10cc68a2230d0014c1ce293bddf9
Shoaib raza <133660631+Thatsshoaib@users.noreply.github.com>
2025-05-31 15:44:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -46,5 +46,6 @@ app.get(/^\/(?!api).*/, (req, res) => {
 const port = 3000;
 const hostname = '0.0.0.0';
 app.listen(port, hostname, () => {
-  console.log('Server running at http://${hostname}:${port}/');
+  console.log(`Server running at http://${hostname}:${port}/`);
+
 });
